f91898c11407aedcd5deea8128448cee542c9423
jcrayton <jcrayton@macalester.edu>
2020-04-22 14:45:28 -0500

    Update main.java
    Added cmd line arg for mining reqard id Java

--- tests/Java/src/main.java
+++ tests/Java/src/main.java
@@ -2,12 +2,17 @@ import java.security.NoSuchAlgorithmException;
 
 public class main {
     public static void main(String[] args) throws NoSuchAlgorithmException {
+        String miningID;
+        if (args[0] != null) miningID = args[0];
+        else miningID = "charity";
         BlockChain sample = new BlockChain(5, 50);
         sample.createGenisisBlock();
+
         Transaction first = new Transaction("Bill", "Bob", 50);
         sample.addPendingTransaction(first);
+
         for(int i = 0; i<= 25; i++) {
-            sample.minePendingTransactions("James");
+            sample.minePendingTransactions(miningID);
         }
 
         sample.printable();
